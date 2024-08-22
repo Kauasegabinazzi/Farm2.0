dc700c875ec0a432ade39d7a9efb7c4eec42f476
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-08-21 23:30:27 -0300

    9-commit

---
+++ Assets/Scripts/Sounds/AudioManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+       
+    }
+}

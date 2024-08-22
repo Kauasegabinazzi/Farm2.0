63c26df8e3fcd91e9cbf628e903c850e4d23ca73
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-08-21 23:40:25 -0300

    10-commit

--- Assets/Scripts/Sounds/AudioManager.cs
+++ Assets/Scripts/Sounds/AudioManager.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    public static AudioManager instance;
 
-       
+    private void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
